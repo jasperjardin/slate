a2f4fb0ec538285d6a211b1a79dc0cb4d6dc5f65
jasperjardin <jasper.jardin1994@gmail.com>
2025-10-24 18:35:20 +0800

    Add Metaboxes initialization file to load all Metabox Classes

---
+++ src/components/metaboxes/initialize.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Loads all the Metabox Classes of the program.
+ *
+ * @package     Slate
+ * @subpackage	Slate/Src/Components/Metaboxes
+ * @category	Slate/Src/Components/Metaboxes/Initialize
+ * @author      Jasper B. Jardin <jasper.jardin1994@gmail.com>
+ * @link        https://profiles.wordpress.org/wpjasper/
+ * @since       0.0.1
+ * @license     GPL-2.0+
+ * @copyright   2025 | https://github.com/jasperjardin/slate
+ * @created_at  2025-10-19
+ */
+
+namespace Slate\Src\Components\Metaboxes;
+
+if ( ! defined( 'ABSPATH' ) ) {
+    return;
+}
